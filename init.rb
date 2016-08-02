90f4b94810121abc5f5ddf4a1c88dce356a59e04
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-08-02 13:33:07 +0300

    url, description added.

--- init.rb
+++ init.rb
@@ -1,8 +1,7 @@
 Redmine::Plugin.register :rest_api do
   name 'Rest Api plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Arcelik AR-GE'
+  description 'This plugin extends the redmine rest api.'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/ARCELIKEI/redmine_plugin_rest_api'
 end
